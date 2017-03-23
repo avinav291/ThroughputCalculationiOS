b4d1925628c6167114b23dbb7f283be2a1cd0e42
avinav291 <avinav291@gmail.com>
2017-03-23 19:10:58 +0530

    Map to find route Map to change terrain and Modes

--- Throughput Calculation/MapTasks.swift
+++ Throughput Calculation/MapTasks.swift
@@ -20,23 +20,44 @@ class MapTasks: NSObject {
  
 	var fetchedAddressLatitude: Double!
 	
+	let baseURLDirections = "https://maps.googleapis.com/maps/api/directions/json?"
+ 
+	var selectedRoute: [String:Any]!
+ 
+	var overviewPolyline: [String:Any]!
+ 
+	var originCoordinate: CLLocationCoordinate2D!
+ 
+	var destinationCoordinate: CLLocationCoordinate2D!
+ 
+	var originAddress: String!
+ 
+	var destinationAddress: String!
+	
+	var totalDistanceInMeters: UInt = 0
+ 
+	var totalDistance: String!
+ 
+	var totalDurationInSeconds: UInt = 0
+ 
+	var totalDuration: String!
+	
 	override init() {
 		super.init()
 	}
 	
+	//MARK:- GEOCODING
+	
 	func geocodeAddress(address: String!, withCompletionHandler completionHandler: @escaping ((_ status: String, _ success: Bool) -> Void)) {
 		if let lookupAddress = address {
-			var geocodeURLString = baseURLGeocode + "address=" + lookupAddress
-			geocodeURLString = geocodeURLString.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-			
-			let geocodeURL = NSURL(string: geocodeURLString)
+			var geocodeURLString = baseURLGeocode+"key=AIzaSyBEKol7xcHOlnhlvNJe9xdwokI8PErEqtA&" + "address=" + lookupAddress
+			geocodeURLString = geocodeURLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
 			
 			DispatchQueue.main.async {
-				let geocodingResultsData = try? Data(contentsOf: geocodeURL! as URL)
+				let geocodingResultsData = try? Data(contentsOf: URL(string: geocodeURLString)!)
 				
 				do{
-					let dictionary = try JSONSerialization.jsonObject(with: geocodingResultsData!, options: .mutableContainers) as! [String:Any]
-					
+					let dictionary = try JSONSerialization.jsonObject(with: geocodingResultsData! as Data, options: .mutableContainers) as! [String:Any]
 					let status = dictionary["status"] as! String
 					
 					if status == "OK" {
@@ -64,4 +85,105 @@ class MapTasks: NSObject {
 			completionHandler("No valid address.", false)
 		}
 	}
+	
+	//MARK:- Direction routing
+	
+	func getDirections(origin: String!, destination: String!, waypoints: Array<String>!, travelMode: TravelModes!, completionHandler: @escaping ((_ status: String, _ success: Bool) -> Void)) {
+		if let originLocation = origin {
+			if let destinationLocation = destination {
+				var directionsURLString = baseURLDirections + "key=AIzaSyBEKol7xcHOlnhlvNJe9xdwokI8PErEqtA&" + "origin=" + originLocation + "&destination=" + destinationLocation
+				
+				if (travelMode) != nil {
+					var travelModeString = ""
+					
+					switch travelMode.rawValue {
+					case TravelModes.walking.rawValue:
+						travelModeString = "walking"
+						
+					case TravelModes.bicycling.rawValue:
+						travelModeString = "bicycling"
+						
+					default:
+						travelModeString = "driving"
+					}
+					directionsURLString += "&mode=" + travelModeString
+				}
+				directionsURLString = directionsURLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+				let directionsURL = NSURL(string: directionsURLString)
+				
+				DispatchQueue.main.async {
+					
+					let directionsData = NSData(contentsOf: directionsURL! as URL)
+					
+					do{
+						let dictionary = try JSONSerialization.jsonObject(with: directionsData! as Data, options: .mutableContainers) as! [String:Any]
+						let status = dictionary["status"] as! String
+						
+						if status == "OK" {
+							self.selectedRoute = (dictionary["routes"] as! [[String:Any]])[0]
+							self.overviewPolyline = self.selectedRoute["overview_polyline"] as! [String:Any]
+							
+							let legs = self.selectedRoute["legs"] as! [[String:Any]]
+							
+							let startLocationDictionary = legs[0]["start_location"] as! [String:Any]
+							self.originCoordinate = CLLocationCoordinate2DMake(startLocationDictionary["lat"] as! Double, startLocationDictionary["lng"] as! Double)
+							
+							let endLocationDictionary = legs[legs.count - 1]["end_location"] as! [String:Any]
+							self.destinationCoordinate = CLLocationCoordinate2DMake(endLocationDictionary["lat"] as! Double, endLocationDictionary["lng"] as! Double)
+							
+							self.originAddress = legs[0]["start_address"] as! String
+							self.destinationAddress = legs[legs.count - 1]["end_address"] as! String
+							
+							self.calculateTotalDistanceAndDuration()
+							
+							completionHandler(status, true)
+						}
+						else {
+							completionHandler(status, false)
+						}
+					}
+					catch{
+						completionHandler("NOT OK", false)
+					}
+					
+					
+				}
+			}
+			else {
+				completionHandler("Destination is nil.", false)
+			}
+		}
+		else {
+			completionHandler("Origin is nil", false)
+		}
+	}
+	
+	func calculateTotalDistanceAndDuration() {
+		let legs = self.selectedRoute["legs"] as! [[String:Any]]
+		
+		totalDistanceInMeters = 0
+		totalDurationInSeconds = 0
+		
+		for leg in legs {
+			totalDistanceInMeters += (leg["distance"] as! [String:Any])["value"] as! UInt
+			totalDurationInSeconds += (leg["duration"] as! [String:Any])["value"] as! UInt
+		}
+		
+		
+		let distanceInKilometers: Double = Double(totalDistanceInMeters / 1000)
+		totalDistance = "Total Distance: \(distanceInKilometers) Km"
+		
+		
+		let mins = totalDurationInSeconds / 60
+		let hours = mins / 60
+		let days = hours / 24
+		let remainingHours = hours % 24
+		let remainingMins = mins % 60
+		let remainingSecs = totalDurationInSeconds % 60
+		
+		totalDuration = "Duration: \(days) d, \(remainingHours) h, \(remainingMins) mins, \(remainingSecs) secs"
+	}
+	
 }
+
+
--- Throughput Calculation/ViewController.swift
+++ Throughput Calculation/ViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+enum TravelModes: Int {
+	case driving
+	case walking
+	case bicycling
+}
+
 class ViewController: UIViewController {
 
 	@IBOutlet weak var viewMap: GMSMapView!
@@ -16,10 +22,23 @@ class ViewController: UIViewController {
 	
 	@IBOutlet weak var lblInfo: UILabel!
 	
+	var mapTasks = MapTasks()
+	
+	var locationMarker: GMSMarker!
+	
 	//Core Location Params
 	var locationManager = CLLocationManager()
  
 	var didFindMyLocation = false
+	
+	var originMarker: GMSMarker!
+ 
+	var destinationMarker: GMSMarker!
+ 
+	var routePolyline: GMSPolyline!
+	
+	//Travel Modes
+	var travelMode = TravelModes.driving
 	
 	
 	override func viewDidLoad() {
@@ -42,10 +61,10 @@ class ViewController: UIViewController {
 	override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
 		if !didFindMyLocation {
 			let myLocation: CLLocation = change![NSKeyValueChangeKey.newKey] as! CLLocation
-			viewMap.camera = GMSCameraPosition.camera(withTarget: myLocation.coordinate, zoom: 10.0)
+			viewMap.camera = GMSCameraPosition.camera(withTarget: myLocation.coordinate, zoom: 15.0)
 			viewMap.settings.myLocationButton = true
 			
-			didFindMyLocation = true
+			didFindMyLocation = false
 		}
 	}
 
@@ -57,14 +76,17 @@ class ViewController: UIViewController {
 		
 		let normalMapTypeAction = UIAlertAction(title: "Normal", style: UIAlertActionStyle.default) { (alertAction) -> Void in
 			self.viewMap.mapType = .normal
+			self.recreateRoute()
 		}
 		
 		let terrainMapTypeAction = UIAlertAction(title: "Terrain", style: UIAlertActionStyle.default) { (alertAction) -> Void in
 			self.viewMap.mapType = .terrain
+			self.recreateRoute()
 		}
 		
 		let hybridMapTypeAction = UIAlertAction(title: "Hybrid", style: UIAlertActionStyle.default) { (alertAction) -> Void in
 			self.viewMap.mapType = .hybrid
+			self.recreateRoute()
 		}
 		
 		let cancelAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
@@ -80,19 +102,202 @@ class ViewController: UIViewController {
 		
 	}
 	
+	func setupLocationMarker(coordinate: CLLocationCoordinate2D) {
+		
+		if locationMarker != nil {
+			locationMarker.map = nil
+		}
+		
+		locationMarker = GMSMarker(position: coordinate)
+		locationMarker.map = viewMap
+		locationMarker.title = mapTasks.fetchedFormattedAddress
+		locationMarker.appearAnimation = .pop
+		locationMarker.icon = GMSMarker.markerImage(with: UIColor.blue)
+		locationMarker.opacity = 0.75
+	}
 	
 	@IBAction func findAddress(sender: AnyObject) {
 		
+		self.clearRoute()
+		
+		let addressAlert = UIAlertController(title: "Address Finder", message: "Type the address you want to find:", preferredStyle: UIAlertControllerStyle.alert)
+		
+		addressAlert.addTextField { (textField) -> Void in
+			textField.placeholder = "Address?"
+		}
+		
+		let findAction = UIAlertAction(title: "Find Address", style: UIAlertActionStyle.default) { (alertAction) -> Void in
+			let address = (addressAlert.textFields![0] as UITextField).text! as String
+			
+			self.mapTasks.geocodeAddress(address: address, withCompletionHandler: { (status, success) -> Void in
+				if !success {
+					print(status)
+					
+					if status == "ZERO_RESULTS" {
+						self.showAlertWithMessage(message: "The location could not be found.")
+					}
+				}
+				else {
+					let coordinate = CLLocationCoordinate2D(latitude: self.mapTasks.fetchedAddressLatitude, longitude: self.mapTasks.fetchedAddressLongitude)
+					self.viewMap.camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 14.0)
+					self.setupLocationMarker(coordinate: coordinate)
+				}
+			})
+			
+		}
+		
+		let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
+			
+		}
+		
+		addressAlert.addAction(findAction)
+		addressAlert.addAction(closeAction)
+		
+		present(addressAlert, animated: true, completion: nil)
+	}
+	
+	func showAlertWithMessage(message: String) {
+		let alertController = UIAlertController(title: "Maps Issue", message: message, preferredStyle: UIAlertControllerStyle.alert)
+		
+		let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
+			
+		}
+		
+		alertController.addAction(closeAction)
+		
+		present(alertController, animated: true, completion: nil)
 	}
 	
 	
 	@IBAction func createRoute(sender: AnyObject) {
+		let addressAlert = UIAlertController(title: "Create Route", message: "Connect locations with a route:", preferredStyle: UIAlertControllerStyle.alert)
 		
+		addressAlert.addTextField { (textField) -> Void in
+			textField.placeholder = "Origin?"
+		}
+		
+		addressAlert.addTextField { (textField) -> Void in
+			textField.placeholder = "Destination?"
+		}
+		
+		let createRouteAction = UIAlertAction(title: "Create Route", style: UIAlertActionStyle.default) { (alertAction) -> Void in
+			if (self.routePolyline) != nil {
+				self.clearRoute()
+			}
+			
+			let origin = (addressAlert.textFields![0] as UITextField).text! as String
+			let destination = (addressAlert.textFields![1] as UITextField).text! as String
+			
+			self.mapTasks.getDirections(origin: origin, destination: destination, waypoints: nil, travelMode: self.travelMode, completionHandler: { (status, success) -> Void in
+				if success {
+					self.configureMapAndMarkersForRoute()
+					self.drawRoute()
+					self.displayRouteInfo()
+				}
+				else {
+					print(status)
+				}
+			})
+		}
+		
+		let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
+			
+		}
+		
+		addressAlert.addAction(createRouteAction)
+		addressAlert.addAction(closeAction)
+		
+		present(addressAlert, animated: true, completion: nil)
 	}
 	
-	
-	@IBAction func changeTravelMode(sender: AnyObject) {
+	func configureMapAndMarkersForRoute() {
+		viewMap.camera = GMSCameraPosition.camera(withTarget: mapTasks.originCoordinate, zoom: 9.0)
 		
+		
+		originMarker = GMSMarker(position: self.mapTasks.originCoordinate)
+		originMarker.map = self.viewMap
+		originMarker.icon = GMSMarker.markerImage(with: UIColor.green)
+		originMarker.title = self.mapTasks.originAddress
+		
+		destinationMarker = GMSMarker(position: self.mapTasks.destinationCoordinate)
+		destinationMarker.map = self.viewMap
+		destinationMarker.icon = GMSMarker.markerImage(with: UIColor.red)
+		destinationMarker.title = self.mapTasks.destinationAddress
+	}
+	
+	func drawRoute() {
+		let route = mapTasks.overviewPolyline["points"] as! String
+		
+		let path: GMSPath = GMSPath(fromEncodedPath: route)!
+		routePolyline = GMSPolyline(path: path)
+		routePolyline.map = viewMap
+	}
+	
+	func displayRouteInfo() {
+		lblInfo.text = mapTasks.totalDistance + "\n" + mapTasks.totalDuration
+	}
+	
+	func clearRoute() {
+		
+		if originMarker != nil{
+			originMarker.map = nil
+		}
+		if destinationMarker != nil{
+			destinationMarker.map = nil
+		}
+		if routePolyline != nil{
+			routePolyline.map = nil
+		}
+
+	}
+	
+	func recreateRoute() {
+		if let polyline = routePolyline {
+			clearRoute()
+			
+			mapTasks.getDirections(origin: mapTasks.originAddress, destination: mapTasks.destinationAddress, waypoints: nil, travelMode: self.travelMode, completionHandler: { (status, success) -> Void in
+				
+				if success {
+					self.configureMapAndMarkersForRoute()
+					self.drawRoute()
+					self.displayRouteInfo()
+				}
+				else {
+					print(status)
+				}
+			})
+		}
+	}
+	
+	//MARK:- Travelling Modes
+	@IBAction func changeTravelMode(sender: AnyObject) {
+		let actionSheet = UIAlertController(title: "Travel Mode", message: "Select travel mode:", preferredStyle: UIAlertControllerStyle.actionSheet)
+		
+		let drivingModeAction = UIAlertAction(title: "Driving", style: UIAlertActionStyle.default) { (alertAction) -> Void in
+			self.travelMode = TravelModes.driving
+			self.recreateRoute()
+		}
+		
+		let walkingModeAction = UIAlertAction(title: "Walking", style: UIAlertActionStyle.default) { (alertAction) -> Void in
+			self.travelMode = TravelModes.walking
+			self.recreateRoute()
+		}
+		
+		let bicyclingModeAction = UIAlertAction(title: "Bicycling", style: UIAlertActionStyle.default) { (alertAction) -> Void in
+			self.travelMode = TravelModes.bicycling
+			self.recreateRoute()
+		}
+		
+		let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
+			
+		}
+		
+		actionSheet.addAction(drivingModeAction)
+		actionSheet.addAction(walkingModeAction)
+		actionSheet.addAction(bicyclingModeAction)
+		actionSheet.addAction(closeAction)
+		
+		present(actionSheet, animated: true, completion: nil)
 	}
 
 
@@ -106,6 +311,10 @@ extension ViewController:CLLocationManagerDelegate{
 		if status == CLAuthorizationStatus.authorizedWhenInUse {
 			viewMap.isMyLocationEnabled = true
 		}
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		
 	}
 	
 }
